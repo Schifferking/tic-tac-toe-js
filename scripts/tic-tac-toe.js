956dced822fff002a3a6f566c94a7a8d3bce93dc
Schifferking <schifferking@hotmail.com>
2023-05-31 12:39:50 -0600

    Feature: let player place marks on the board
    In HTML, the buttons have data-attributes to easily identify them.
    
    In Javascript, moved some objects. There are functions to start a new
    round, update the h2 element, get the current player, udpate the current
    player, add listeners to the buttons (e.g. board cells), etc.
    
    All these functions let a player place a mark on a cell only once, like
    the original game. It also updates the board array to later check if
    a game ends in win or draw.

--- scripts/tic-tac-toe.js
+++ scripts/tic-tac-toe.js
@@ -1,24 +1,51 @@
 const gameBoard = (() => {
-  const board = new Array(9).fill('O');
-  return { board };
-})();
-
-const gameLogic = (() => {
-  /* Logic goes here */
-})();
-
-const displayController = (() => {
-  const buttons = document.querySelectorAll(".board-container > div > button");
-  const renderGameBoard = (gameBoard) => {
-    index = 0;
-    for (let button of buttons) {
-      button.textContent = gameBoard[index];
-      index++;
-    }
-  };
-  return { renderGameBoard };
+  const _board = new Array(9).fill('');
+  const getBoard = () => _board;
+  const placeMark = (index, mark) => _board[index] = mark;
+  return { getBoard, placeMark };
 })();
 
 const playerFactory = (mark) => {
-  return { mark };
+  const getMark = () => mark;
+  return { getMark };
 };
+
+const displayController = (() => {
+  const _h2 = document.querySelector("h2");
+  const updateH2 = (text) => _h2.textContent = text;
+  return { updateH2 };
+})();
+
+const gameLogic = (() => {
+  const _buttons = document.querySelectorAll(".board-container > div > button");
+  const _playerX = playerFactory('X'); 
+  const _playerO = playerFactory('O'); 
+  let _currentPlayer = _playerX;
+  const _updateCurrentPlayer = () => {
+    _currentPlayer = _currentPlayer === _playerX ? _playerO : _playerX;
+  };
+  const _getCurrentPlayer = () => _currentPlayer;
+  const _placeMark = (button) => {
+    if (button.textContent === '') {
+      index = Number(button.getAttribute("data-index"));
+      mark = _getCurrentPlayer().getMark();
+      gameBoard.placeMark(index, mark);
+      button.textContent = mark;
+      _startNewRound();
+    } else return;
+  };
+  const _startNewRound = () => {
+    _updateCurrentPlayer();
+    displayController.updateH2(`${_currentPlayer.getMark()} player's turn`);
+  };
+  const setButtonsListeners = () => {
+    for (let button of _buttons) {
+      button.addEventListener("click", function(event){
+        _placeMark(event.target);
+      });
+    }
+  };
+  return { setButtonsListeners };
+})();
+
+gameLogic.setButtonsListeners();
