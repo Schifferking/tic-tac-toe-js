059508c16b25e739f65134f280ea94406b9b8c45
Schifferking <schifferking@hotmail.com>
2023-06-01 18:50:07 -0600

    Feature: let players enter names and start game
    There is a new form that lets players enter their names before playing.
    There is also a new button to start and restart a game. The h2 element
    that displays the current player's turn is also used to displayed the
    result of a game.

--- scripts/tic-tac-toe.js
+++ scripts/tic-tac-toe.js
@@ -1,8 +1,8 @@
 const gameBoard = (() => {
-  const _board = new Array(9).fill('');
+  let _board = new Array(9).fill('');
   const getBoard = () => _board;
   const placeMark = (index, mark) => _board[index] = mark;
-  const _getRows = () =>{
+  const _getRows = () => {
     return [_board.slice(0, 3), _board.slice(3, 6), _board.slice(6)];
   };
   const _getColumns = () => {
@@ -18,12 +18,14 @@ const gameBoard = (() => {
     return _getRows().concat(_getColumns(), _getDiagonals());
   };
   const isNotBoardFull = () => _board.includes('');
-  return { getBoard, getLines, isNotBoardFull, placeMark };
+  const resetBoard = () => _board = new Array(9).fill('');
+  return { getBoard, getLines, isNotBoardFull, placeMark, resetBoard };
 })();
 
-const playerFactory = (mark) => {
+const playerFactory = (name, mark) => {
+  const getName = () => name;
   const getMark = () => mark;
-  return { getMark };
+  return { getName, getMark };
 };
 
 const displayController = (() => {
@@ -33,45 +35,107 @@ const displayController = (() => {
 })();
 
 const gameLogic = (() => {
+  const _startButton = document.querySelector("#start-game");
   const _buttons = document.querySelectorAll(".board-container > div > button");
-  const _playerX = playerFactory('X'); 
-  const _playerO = playerFactory('O'); 
-  let _currentPlayer = _playerX;
-  const _updateCurrentPlayer = () => {
-    _currentPlayer = _currentPlayer === _playerX ? _playerO : _playerX;
+  const _form = document.querySelector("[name='players']");
+  let _firstPlayer = {}; 
+  let _secondPlayer = {};
+  let _currentPlayer = {};
+  let _gameStart = false;
+  let _buttonsSet = false;
+  const _disableButtons = () => {
+    for (let button of _buttons)
+      button.disabled = true;
   };
-  const _getCurrentPlayer = () => _currentPlayer;
+  const _activateButtons = () => {
+    for (let button of _buttons)
+      button.disabled = false;
+  };
+  const _resetButtons = () => {
+    for (let button of _buttons)
+      button.textContent = '';
+  };
+  const _createPlayers = () => {
+    _form.addEventListener("submit", function(event) {
+      const firstPlayerName = event.currentTarget.querySelector("[name='ttt-first-player-name']").value;
+      const secondPlayerName = event.currentTarget.querySelector("[name='ttt-second-player-name']").value;
+      _firstPlayer = playerFactory(firstPlayerName, 'X');
+      _secondPlayer = playerFactory(secondPlayerName, 'O');
+      _currentPlayer = _firstPlayer;
+      if (Object.keys(_firstPlayer).length > 0)
+        _startButton.disabled = false;
+      event.preventDefault();
+    });
+  };
+  const _updateCurrentPlayer = () => {
+    _currentPlayer = _currentPlayer === _firstPlayer ? _secondPlayer : _firstPlayer;
+  };
   const _isGameVictory = (line, mark) => {
     return JSON.stringify(line) === JSON.stringify(new Array(3).fill(mark)) ? true : false;
   };
-  const gameResult = () => {
+  const _gameResult = () => {
     for (let line of gameBoard.getLines()) {
-      if (_isGameVictory(line, mark)) return `Player ${mark} won the game`;
+      if (_isGameVictory(line, _currentPlayer.getMark()))
+        return `${_currentPlayer.getName()} won the game, congratulations`;
     }
-    if (gameBoard.isNotBoardFull() === false) return 'Draw';
+    if (gameBoard.isNotBoardFull() === false)
+      return 'Draw';
   };
   const _placeMark = (button) => {
     if (button.textContent === '') {
       index = Number(button.getAttribute("data-index"));
-      mark = _getCurrentPlayer().getMark();
+      mark = _currentPlayer.getMark();
       gameBoard.placeMark(index, mark);
       button.textContent = mark;
-      _startNewRound();
     } else return;
   };
   const _startNewRound = () => {
     _updateCurrentPlayer();
-    displayController.updateH2(`Player ${_currentPlayer.getMark()}'s turn`);
+    displayController.updateH2(`${_currentPlayer.getName()}'s turn`);
   };
-  const setButtonsListeners = () => {
+  const _setButtonsListeners = () => {
     for (let button of _buttons) {
       button.addEventListener("click", function(event){
         _placeMark(event.target);
-        console.log(gameResult());
+        let result = _gameResult();
+        event.target.disabled = true;
+        if (result) {
+          displayController.updateH2(result);
+          _disableButtons();
+          return;
+        }
+        _startNewRound();
       });
     }
   };
-  return { gameResult, setButtonsListeners };
+  const _restartGame = () => {
+    _startButton.disabled = true;
+    _gameStart = false;
+    _firstPlayer = {};
+    _secondPlayer = {};
+    _currentPlayer = {};
+    _resetButtons();
+    _disableButtons();
+    gameBoard.resetBoard();
+  };
+  const startGame = () => {
+    _disableButtons();
+    _createPlayers();
+    _startButton.disabled = true;
+    _startButton.addEventListener("click", function(event) {
+      if (_gameStart === false) {
+        _gameStart = true;
+        _activateButtons();
+        // prevent duplicate event listeners
+        if (_buttonsSet === false) {
+          _setButtonsListeners();
+          _buttonsSet = true;
+        }
+        displayController.updateH2(`${_currentPlayer.getName()}'s turn`);
+      } else _restartGame();
+    });
+  };
+  return { startGame };
 })();
 
-gameLogic.setButtonsListeners();
+gameLogic.startGame();
